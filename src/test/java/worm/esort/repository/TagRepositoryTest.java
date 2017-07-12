59b36de0482fe4fb1c52d0e3fbf0c61462d01143
garview <garview@foxmail.com>
2017-07-13 01:08:14 +0800

    添加测试代码；

---
+++ src/test/java/worm/esort/repository/TagRepositoryTest.java
@@ -0,0 +1,23 @@
+package worm.esort.repository;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest 
+public class TagRepositoryTest {
+	
+	@Autowired
+	TagRepository tagResp;
+	
+	@Test
+	public void testFlow() {
+		Assert.assertTrue(tagResp.count()==0);
+	}
+
+
+}
---
+++ src/test/java/worm/esort/service/EWormIndexServiceTest.java
@@ -0,0 +1,35 @@
+package worm.esort.service;
+
+import java.io.IOException;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class EWormIndexServiceTest {
+
+	@Resource(name="eWormIndexService")
+	EWormIndexService eWormIndexService; 
+	@Test
+	public void testCrawlSearchResult() throws IOException {
+		String originUrl = "https://e-hentai.org/?f_doujinshi=1&f_manga=1&f_artistcg=1&f_gamecg=1&f_western=1&f_non-h=1&f_imageset=1&f_cosplay=1&f_asianporn=1&f_misc=1&f_search=chinese&f_apply=Apply+Filter";
+		eWormIndexService.crawlSearchResult(originUrl);;
+	}
+//
+//	@Test
+//	public void testCrawlListPageString() {
+//		fail("Not yet implemented");
+//	}
+//
+
+	@Test
+	public void testCrawlBook() {
+		eWormIndexService.crawlBook("https://e-hentai.org/g/1086305/4d5acc0a41/");;
+	}
+
+}
