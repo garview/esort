d650aec54996b57a35ae954649d95259ed733a4a
garview <garview@foxmail.com>
2017-07-13 23:16:13 +0800

    日志输出参数化例子

--- src/test/java/worm/esort/AppTest.java
+++ src/test/java/worm/esort/AppTest.java
@@ -1,38 +1,14 @@
 package worm.esort;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+	private static final Logger logger = LogManager.getLogger(AppTest.class);
+	@Test
+	public void test() {
+		logger.info("as[{}]d","sdfs");
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
