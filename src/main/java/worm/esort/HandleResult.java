9ec3b73408789b70de9e7237cf00a78b2f1c13e0
garview <garview@foxmail.com>
2017-08-08 12:28:36 +0800

    修正时间估算算法；

--- src/main/java/worm/esort/HandleResult.java
+++ src/main/java/worm/esort/HandleResult.java
@@ -13,6 +13,8 @@ public class HandleResult {
 	private static final Logger logger = LogManager.getLogger();
 
 	private Integer totalPageCount = 0;
+	
+	private float totalCostTime = 0;
 
 	public List<Integer> handledPages = new ArrayList<Integer>();
 
@@ -39,12 +41,19 @@ public class HandleResult {
 	 * 估算剩余时间
 	 * @param pageCostTime 每页耗时（秒）
 	 */
-	public void expectRemainTime(float pageCostTime) {
-		float remainTime = (((totalPageCount - handledPages.size())/2) * pageCostTime);// 剩余时间（秒）
-		int hours = (int) (remainTime / 3600);
-		int minutes = (int) ((remainTime % 3600f) / 60);
-		int seconds = (int) ((remainTime % 3600f) % 60);
-		logger.info("进度：{}/{}, 剩余{}小时{}分钟{}秒", handledPages.size(),totalPageCount,hours, minutes, seconds);
+	public synchronized void expectRemainTime(float pageCostTime) {
+		totalCostTime+=pageCostTime;
+		float everyPageCost = totalCostTime/handledPages.size();
+		float remainTime = everyPageCost*(totalPageCount - handledPages.size());
+//		float remainTime = (((totalPageCount - handledPages.size())/2) * pageCostTime);// 剩余时间（秒）
+		logger.info("进度：{}/{},已耗时:{}, 预计剩余:{}", handledPages.size(),totalPageCount,convertSeconds2HMS(totalCostTime),convertSeconds2HMS(remainTime));
+	}
+	
+	private String convertSeconds2HMS(float time){
+		int hours = (int) (time / 3600);
+		int minutes = (int) ((time % 3600f) / 60);
+		int seconds = (int) ((time % 3600f) % 60);
+		return hours+"时"+minutes+"分"+seconds+"秒";
 	}
 
 //	public static void testExpectRemainTime(){
