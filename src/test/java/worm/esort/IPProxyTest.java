960c628b5a8afa3f926b4118699d406c21648156
garview <garview@garview-Lenovo-G40-70m>
2017-08-25 01:29:52 +0800

    测试代理的另一种写法——system.setProperty的方式有两个坑：1. 慢 2. 用代理连接失败后，不是抛出异常，而是使用本机ip继续连。这就是我ip被封的原因了……

--- src/test/java/worm/esort/IPProxyTest.java
+++ src/test/java/worm/esort/IPProxyTest.java
@@ -1,6 +1,8 @@
 package worm.esort;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -79,11 +81,14 @@ public class IPProxyTest {
 	}
 	@Test
 	public void testTimeout() throws IOException {
-		System.setProperty("proxySet", "true");
-		 System.setProperty("http.proxyHost", "222.52.142.242");
-		 System.setProperty("http.proxyPort", "8080");
+//		System.setProperty("proxySet", "true");
+//		 System.setProperty("https.proxyHost", "110.243.166.164");
+//		 System.setProperty("https.proxyPort", "80");
+//		 System.setProperty("http.proxyHost", "110.243.166.164");
+//		 System.setProperty("http.proxyPort", "80");
+//		 Proxy p = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("110.243.166.164", 80));
 		 long t1 = System.currentTimeMillis();
-			Document doc = Jsoup.connect("http://www.baidu.com").get();
+			Document doc = Jsoup.connect("https://www.baidu.com/s?ie=UTF-8&wd=ip")/*.proxy(p)*/.get();
 			long t2 = System.currentTimeMillis();
 			System.out.println(doc);
 			System.out.println((t2-t1)/1000f+"秒");
